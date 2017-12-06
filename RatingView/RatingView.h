055389e1dfc3174b4373a434257cb9834c224662
Christopher Shireman <chris@shireman.net>
2017-12-06 11:20:34 -0800

    Reworking framework structure

---
+++ RatingView/RatingView.h
@@ -0,0 +1,20 @@
+//
+//  RatingView.h
+//  RatingView
+//
+//  Created by Christopher Shireman on 12/6/17.
+//  Copyright © 2017 Selim Bakdemir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RatingView.
+FOUNDATION_EXPORT double RatingViewVersionNumber;
+
+//! Project version string for RatingView.
+FOUNDATION_EXPORT const unsigned char RatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RatingView/PublicHeader.h>
+
+#import <RatingView/SRatingView.h>
+
